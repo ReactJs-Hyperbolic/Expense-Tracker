27b193af2dcac58076ba20ddeed9ed39adf71e2a
Karsenh <KarsenHansen@gmail.com>
2021-04-11 00:58:31 -0700

    Added Header, Balance, IncomeExpenses, and transactionList components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
